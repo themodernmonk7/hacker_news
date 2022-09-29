569c2265acb728c5c68d7dc3363d858d7edead55
themodernmonk7 <sauravavishek@gmail.com>
2022-09-29 16:36:47 +0530

    01-constant actions file

--- src/actions.jsx
+++ src/actions.jsx
@@ -1,1 +1,5 @@
-
+export const SET_LOADING = "SET_LOADING"
+export const SET_STORIES = "SET_STORIES"
+export const HANDLE_SEARCH = "HANDLE_SEARCH"
+export const HANDLE_PAGE = "HANDLE_PAGE"
+export const REMOVE_STORIES = "REMOVE_STORIES"
