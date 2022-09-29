d2dfeccc059f9f16c19cf34b8090c9f9ba856fc6
themodernmonk7 <sauravavishek@gmail.com>
2022-09-29 19:29:07 +0530

    Prev and Next button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,14 +1,26 @@
 import React from "react"
+import { useGlobalContext } from "../context/context"
 
 const Buttons = () => {
+  const { loading, handlePage, page, nbPages } = useGlobalContext()
   return (
     <>
       <div className="container mx-auto flex justify-center items-center space-x-6 xl:text-xl">
-        <button className="border text-white px-3 text-sm rounded-sm bg-gradient-to-r from-[#e052a0] to-[#f15c41] xl:text-xl ">
+        <button
+          className="border text-white px-3 text-sm rounded-sm bg-gradient-to-r from-[#e052a0] to-[#f15c41] xl:text-xl disabled:cursor-not-allowed "
+          disabled={loading}
+          onClick={() => handlePage("decrease")}
+        >
           Prev
         </button>
-        <p>1 of 50 </p>
-        <button className="border text-white px-3 text-sm rounded-sm bg-gradient-to-r from-[#e052a0] to-[#f15c41] xl:text-xl">
+        <p>
+          {page + 1} of {nbPages}{" "}
+        </p>
+        <button
+          className="border text-white px-3 text-sm rounded-sm bg-gradient-to-r from-[#e052a0] to-[#f15c41] xl:text-xl disabled:cursor-not-allowed"
+          disabled={loading}
+          onClick={() => handlePage("increase")}
+        >
           Next
         </button>
       </div>
--- src/reducer/reducer.jsx
+++ src/reducer/reducer.jsx
@@ -25,6 +25,22 @@ const reducer = (state, action) => {
     case HANDLE_SEARCH: {
       return { ...state, query: action.payload, page: 0 }
     }
+    case HANDLE_PAGE: {
+      if (action.payload === "increase") {
+        let nextPage = state.page + 1
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0
+        }
+        return { ...state, page: nextPage }
+      }
+      if (action.payload === "decrease") {
+        let prevPage = state.page - 1
+        if (prevPage < 0) {
+          prevPage = state.nbPages - 1
+        }
+        return { ...state, page: prevPage }
+      }
+    }
 
     default:
       throw new Error(`not matching "${action.type}" action type`)
